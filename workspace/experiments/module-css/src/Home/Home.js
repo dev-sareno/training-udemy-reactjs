74a53135d8df227d36ef7040ab0fd96087495618
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-23 22:13:18 +0800

    chore(css-module): Adding more experiments | Two level css selector

--- workspace/experiments/module-css/src/Home/Home.js
+++ workspace/experiments/module-css/src/Home/Home.js
@@ -6,6 +6,23 @@ const homeComponent = () => {
         <div>
             <h1 className={styles.headerOne}>Home</h1>
             <p className={styles.parag}>Est consequat dolore occaecat laborum incididunt adipisicing proident ea sunt.</p>
+            <div className={styles.myDiv}>
+                <p className={styles.parag01}>
+                    Officia non commodo eiusmod fugiat incididunt officia consectetur cupidatat incididunt ullamco et adipisicing. Incididunt sit reprehenderit ex consequat excepteur ullamco nostrud cupidatat deserunt minim elit anim sit. Mollit eiusmod ea pariatur officia esse ut culpa ipsum ullamco ipsum minim laborum et est. Id magna minim cillum pariatur amet ut velit.
+
+                    Cillum eu et et excepteur irure Lorem mollit elit dolore do labore adipisicing proident. Sunt et minim veniam sint. Deserunt ullamco laboris do consequat elit voluptate veniam cupidatat. Eiusmod proident proident veniam voluptate enim irure voluptate amet mollit cillum irure. In minim ipsum consequat commodo duis dolor magna. Aliquip ut aute sunt duis ad laborum non ipsum sint nulla.
+
+                    Consectetur tempor Lorem anim mollit deserunt. Duis commodo amet exercitation elit ipsum sint laborum cillum duis. Eu id do laboris officia ex aute nostrud ut nulla sint.
+                </p>
+                
+                <p className={styles.parag02}>
+                    Officia non commodo eiusmod fugiat incididunt officia consectetur cupidatat incididunt ullamco et adipisicing. Incididunt sit reprehenderit ex consequat excepteur ullamco nostrud cupidatat deserunt minim elit anim sit. Mollit eiusmod ea pariatur officia esse ut culpa ipsum ullamco ipsum minim laborum et est. Id magna minim cillum pariatur amet ut velit.
+
+                    Cillum eu et et excepteur irure Lorem mollit elit dolore do labore adipisicing proident. Sunt et minim veniam sint. Deserunt ullamco laboris do consequat elit voluptate veniam cupidatat. Eiusmod proident proident veniam voluptate enim irure voluptate amet mollit cillum irure. In minim ipsum consequat commodo duis dolor magna. Aliquip ut aute sunt duis ad laborum non ipsum sint nulla.
+
+                    Consectetur tempor Lorem anim mollit deserunt. Duis commodo amet exercitation elit ipsum sint laborum cillum duis. Eu id do laboris officia ex aute nostrud ut nulla sint.
+                </p>
+            </div>
         </div>
     );
 }
