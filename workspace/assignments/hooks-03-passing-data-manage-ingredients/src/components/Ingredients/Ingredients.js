bddee7941265deabe167d714ab2f89f29c6e87f3
netzon-eladio <eladio.sareno@netzontech.com>
2020-11-30 10:06:33 +0800

    chore(assignment): Completed the assignment 06 | Hooks

---
+++ workspace/assignments/hooks-03-passing-data-manage-ingredients/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    const updatedIngredients = userIngredients.filter(ig => ig.id !== ingredientId);
+    setUserIngredients(updatedIngredients);
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
